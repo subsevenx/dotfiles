8735959aaf019ddd83f8127926234f5bd342086d
subsevenx <daniel.hernandez@monva.io>
2025-10-24 14:08:21 -0500

    Typeface

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(global-display-line-numbers-mode t)
  '(package-selected-packages
    '(company counsel doom-themes exec-path-from-shell flycheck ivy
 	     lsp-mode)))
@@ -11,4 +12,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "FiraCode Nerd Font Propo" :foundry "CTDB" :slant normal :weight medium :height 143 :width normal)))))
