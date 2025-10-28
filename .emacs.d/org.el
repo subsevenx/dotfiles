a7b50e168e5e88f4dc45b41de515049c3b682d40
subsevenx <daniel.hernandez@monva.io>
2025-10-28 12:03:09 -0500

    Org config changes

--- .emacs.d/org.el
+++ .emacs.d/org.el
@@ -1,7 +1,10 @@
 
 ;;; Code:
+(setq org-hide-emphasis-markers t)
 
-
+(font-lock-add-keywords 'org-mode
+			'(("^ *\\([-]\\) "
+			   (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 
 (provide 'org)
 
