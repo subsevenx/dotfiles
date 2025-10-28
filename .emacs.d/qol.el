d27ecf53947b801b3189c7235357028bb8f7e949
subsevenx <daniel.hernandez@monva.io>
2025-10-27 19:13:46 -0500

    Documentation

--- .emacs.d/qol.el
+++ .emacs.d/qol.el
@@ -2,14 +2,19 @@
 ;;; Code:
 
 ;; Quality of life options
-(set-face-attribute 'default nil :height 220)
+(set-face-attribute 'default nil :height 200) ; Font size
 
 (setq inhibit-startup-message t) ; No startup message
 
+;; GUI
 (scroll-bar-mode -1) ; Hide vert. scrollbar
 (horizontal-scroll-bar-mode -1) ; Hide horizontal scroll bar
+(tool-bar-mode -1) ; Hides the toolbar
 (add-to-list 'default-frame-alist '(fullscreen . maximized)) ; Set to full screen ASAP
+(set-fringe-mode 10) ; Setting gutters
 (global-display-line-numbers-mode t) ; Show line numbers
+
+;; Editing
 (save-place-mode) ; Save cursor place in file exit
 (global-hl-line-mode t) ; Highlights current line
 (show-paren-mode t) ; Shows the paired parentheses
@@ -21,7 +26,7 @@
 		eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-(defalias 'yes-or-no-p 'y-or-n-p)
+(defalias 'yes-or-no-p 'y-or-n-p) ; Cuts down the alias to y or n
 
 (provide 'qol)
 ;;; qol.el ends here
