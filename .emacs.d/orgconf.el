dc374907a23de3f130e1af9076ea38a73ccaaec6
subsevenx <daniel.hernandez@monva.io>
2025-10-28 16:09:30 -0500

    Refactored to use hooks

--- .emacs.d/orgconf.el
+++ .emacs.d/orgconf.el
@@ -1,5 +1,24 @@
 
 ;;; Code:
+
+;; Make use of latest org-mode version
+(use-package org
+  :hook (org-mode . org-mode-setup)
+  :config
+  (setq org-ellipsis " ◇"
+	org-hide-emphasis-markers t))
+
+;; Unicode Bullets for Org Mode
+(use-package org-bullets)
+
+; I have to hook so options get added to run-time
+(defun org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (auto-fill-mode 0)
+  (visual-line-mode 1)
+  (org-bullets-mode 1))
+
 (font-lock-add-keywords 'org-mode
 			'(("^ *\\([-]\\) "
 			   (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
