addded10712bf62746a641a50ce7aa8a28ac203f
subsevenx <daniel.hernandez@monva.io>
2025-10-28 23:05:42 -0500

    Deleted stable source

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,7 +11,6 @@
 (require 'package)
 (setq package-archives
       '(("melpa" . "https://melpa.org/packages/")
-	("melpa-stable" . "https://stable.melpa.org/packages/")
 	("org" . "https://orgmode.org/elpa/")
         ("gnu" . "https://elpa.gnu.org/packages/")
         ("nongnu" . "https://elpa.nongnu.org/packages/")))
