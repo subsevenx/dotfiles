f816f3756ee98e9d287966eaf306c4b9d17bc461
subsevenx <daniel.hernandez@monva.io>
2025-11-05 20:01:56 -0600

    Auto-updater

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,7 +4,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(all-the-icons auto-package-update company-box counsel doom-modeline
+		   doom-themes exec-path-from-shell flycheck helpful
+		   ivy-prescient ivy-rich lsp-mode no-littering
+		   org-superstar projectile visual-fill-column)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,6 +22,15 @@
 
 (require 'use-package)
 
+(use-package auto-package-update
+  :custom
+  (auto-package-update-interval 7)
+  (auto-package-update-prompt-before-update t)
+  (auto-package-update-hide-results t)
+  :config
+  (auto-package-update-maybe)
+  (auto-package-update-at-time "09:00"))
+
 (setq
  use-package-always-ensure t
  use-package-verbose t)
