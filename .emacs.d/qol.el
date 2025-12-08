7e41ee372b0804c8f6537977422477e479b457b3
subsevenx <daniel.hernandez@monva.io>
2025-12-08 11:30:04 -0600

    Lowered font size

--- .emacs.d/qol.el
+++ .emacs.d/qol.el
@@ -4,13 +4,13 @@
 ;;; Description: Quality of life options
 
 ;; Fonts:
-(set-face-attribute 'default nil :font "Fira Code" :height 170)
+(set-face-attribute 'default nil :font "Fira Code" :height 165)
 
 ;; Set the fixed pitch face
-(set-face-attribute 'fixed-pitch nil :font "Fira Code" :height 170)
+(set-face-attribute 'fixed-pitch nil :font "Fira Code" :height 165)
 
 ;; Set the variable pitch face
-(set-face-attribute 'variable-pitch nil :font "Droid Sans" :height 170 :weight 'regular)
+(set-face-attribute 'variable-pitch nil :font "Droid Sans" :height 165 :weight 'regular)
 
 (setq inhibit-compacting-font-caches t)
 
